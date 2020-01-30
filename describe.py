c954115dfde4e6fece3afdc67422ce5da77f9cd9
Luyanda Mncube (Britehouse) <luyanda.mncube@britehouse.co.za>
2020-01-30 20:37:11 +0200

    Minor cleanup

--- describe.py
+++ describe.py
@@ -1,28 +1,25 @@
-import sys 
 import pandas as pd
-
+import sys
 df = pd.DataFrame()
 
-def readData():
-    global df
-    if (len(sys.argv) != 2):
-        print('USAGE: python describe.py dataset.csv')
+# csvData = sys.argv
+def readData(csvData):
+    if (len(csvData) != 2):
+        print('USAGE: python <SCRIPTNAME>.py dataset.csv')
         exit()
-    elif (not sys.argv[1].endswith('.csv')):
+    elif (not csvData[1].endswith('.csv')):
         print("ERROR: dataset not in csv format")
         exit()
     else:
         try:
             # Read csv and remove index column
-            df = pd.read_csv(sys.argv[1])
-            df = df.drop(['Index'],axis=1)
-            # df.set_index('Index', inplace=True)
-            # print(df.head(5))
+            df = pd.read_csv(csvData[1])
+            # df = df.drop(['Index'],axis=1)
+            return (df)
         except Exception as e:
             print(f'ERROR: {e}')
 
-def describe():
-    global df
+def describe(df):
     # find and select numeric types in dataframe
     numeric = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     df = df.select_dtypes(include=numeric)
@@ -38,19 +35,15 @@ def describe():
         df.quantile(0.75),  
         df.max(axis=0)    
         ], axis=1)
-    # rename columns 
+
+    # Rename columns 
     df.columns = ['Count','Mean', 'Std', 'Min','25%','50%','75%','Max']
     
     df = df.transpose()
-    # rename column names
-    count = 0
-    for name, values in df.iteritems():
-        count += 1
-        df.rename({ name: 'Feature ' + str(count)}, axis=1, inplace=True)
-
     print(df)
+    return(df)
 
 if __name__ == "__main__":
-    readData()
-    print('SUCCESS: Data read')
-    describe()
+    df =  readData(sys.argv)
+    # print('SUCCESS: Data read')
+    describe(df)
