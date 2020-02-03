ac5e7634e95846af5df2ad68877535b4712d9409
Luyanda Mncube (Britehouse) <luyanda.mncube@britehouse.co.za>
2020-02-03 19:07:08 +0200

    Cleanup

--- describe.py
+++ describe.py
@@ -1,24 +1,9 @@
-import pandas as pd
+
 import sys
+import pandas as pd
+from helpers import readData
+
 df = pd.DataFrame()
-
-# csvData = sys.argv
-def readData(csvData):
-    if (len(csvData) != 2):
-        print('USAGE: python <SCRIPTNAME>.py dataset.csv')
-        exit()
-    elif (not csvData[1].endswith('.csv')):
-        print("ERROR: dataset not in csv format")
-        exit()
-    else:
-        try:
-            # Read csv and remove index column
-            df = pd.read_csv(csvData[1])
-            # df = df.drop(['Index'],axis=1)
-            return (df)
-        except Exception as e:
-            print(f'ERROR: {e}')
-
 def describe(df):
     # find and select numeric types in dataframe
     numeric = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
